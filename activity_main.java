401aa62519510a1f67af37ea57283e08ed71ad6c
Chandranshu Jha <64300705+chandranshu218@users.noreply.github.com>
2022-08-05 11:40:45 +0530

    Update activity_main.java

--- activity_main.java
+++ activity_main.java
@@ -1,104 +1,38 @@
-package com.example.galleryapp;
+package com.example.program4b;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.io.InputStream;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity
-{
-       private static final int REQUEST_CODE_STORAGE_PERMISSION = 1;
-       private static final int REQUEST_CODE_SELECT_IMAGE = 2;
-
-       private ImageView imageSelected;
-
+public class MainActivity extends AppCompatActivity {
+    private final int MY_REQUEST_CODE=1;
+    ImageView imgGallery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageSelected = findViewById(R.id.selectedImage);
+        imgGallery=findViewById(R.id.imgGallery);
+        Button btnGallery=findViewById(R.id.btnGallery);
 
-        findViewById(R.id.buttonSelectedImage).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (ContextCompat.checkSelfPermission(
-                        getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(
-                            MainActivity.this,
-                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                            REQUEST_CODE_STORAGE_PERMISSION);
-                } else {
-                    selectImage();
-
-                }
-            }
-
-                    });
-
-                }
-
-
-    private void selectImage()
-    {
-    Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-    if(intent.resolveActivity(getPackageManager()) != null){
-        startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
+        btnGallery.setOnClickListener(v -> {
+            Intent iGallery=new Intent(Intent.ACTION_PICK);
+            iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(iGallery,MY_REQUEST_CODE);
+        });
     }
-    }
-    @Override
-       public void onRequestPermissionsResult(int requestCode,@Nullable String[] permissions,@Nullable int[] grantResults)
-        {
-            super.onRequestPermissionsResult(requestCode,permissions,grantResults);
-
-            if(requestCode == REQUEST_CODE_STORAGE_PERMISSION && grantResults.length > 0)
-            {
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    selectImage();
-                }
-                else
-                {
-                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_CODE_SELECT_IMAGE && resultCode == RESULT_OK){
-            if(data != null){
-                Uri selectedImageUri = data.getData();
-                if(selectedImageUri != null){
-                    try{
-
-                        InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
-                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                        imageSelected.setImageBitmap(bitmap);
-                    }catch (Exception exception){
-                        Toast.makeText(this,exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
+        if(resultCode==RESULT_OK){
+            if(requestCode==MY_REQUEST_CODE){
+                assert data != null;
+                imgGallery.setImageURI(data.getData());
             }
         }
     }
